6e4a0d7bf70e889f5b14d73cca4eaeff0e2b36e8
EliasGabriel1 <elias.147682@hotmail.com>
2022-04-07 21:12:03 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { createTable, insertPessoa, updatePessoa, selectPessoas, selectPessoa, deletePessoa } from "./Controler/pessoa.js";
+import { createTable, insertPessoa, updatePessoa, selectPessoas, selectPessoa, deletePessoa } from "./Controler/Pessoa.js";
 import { createTableCaixa, selectTodos, selectTodosValidated , selectConta, selectValorPessoa} from "./Controler/Caixa.js";
+// import { createTableCaixa, selectTodos, selectTodosValidated , selectConta, selectValorPessoa} from "./Controler/Cliente.js";
+// import { createTableCaixa, selectTodos, selectTodosValidated , selectConta, selectValorPessoa} from "./Controler/Entrada.js";
+// import { createTableCaixa, selectTodos, selectTodosValidated , selectConta, selectValorPessoa} from "./Controler/Produto.js";
+// import { createTableCaixa, selectTodos, selectTodosValidated , selectConta, selectValorPessoa} from "./Controler/Saida.js";
 
 const router = Router();
 
 createTable();
 createTableCaixa();
+
 
 router.get('/', (req, res)=> {
     res.json({
@@ -15,17 +20,53 @@ router.get('/', (req, res)=> {
     }
 )
 
+
+
+// CAIXA
 router.get('/Todos', selectTodos)
 router.get('/TodosValidado', selectTodosValidated)
 router.get('/Conta', selectConta)
 router.get('/ValorPessoa', selectValorPessoa)
 
 
-
+// PESSOA
 router.get('/pessoas', selectPessoas)
 router.get('/pessoa', selectPessoa)
 router.post('/pessoa', insertPessoa)
 router.put('/pessoa', updatePessoa)
 router.delete('/pessoa', deletePessoa)
+
+
+// CLIENTE
+// router.get('/pessoas', selectPessoas)
+// router.get('/pessoa', selectPessoa)
+// router.post('/pessoa', insertPessoa)
+// router.put('/pessoa', updatePessoa)
+// router.delete('/pessoa', deletePessoa)
+
+
+// ENTRADA
+// router.get('/pessoas', selectPessoas)
+// router.get('/pessoa', selectPessoa)
+// router.post('/pessoa', insertPessoa)
+// router.put('/pessoa', updatePessoa)
+// router.delete('/pessoa', deletePessoa)
+
+
+// PRODUTO
+// router.get('/pessoas', selectPessoas)
+// router.get('/pessoa', selectPessoa)
+// router.post('/pessoa', insertPessoa)
+// router.put('/pessoa', updatePessoa)
+// router.delete('/pessoa', deletePessoa)
+
+
+// SAIDA
+// router.get('/pessoas', selectPessoas)
+// router.get('/pessoa', selectPessoa)
+// router.post('/pessoa', insertPessoa)
+// router.put('/pessoa', updatePessoa)
+// router.delete('/pessoa', deletePessoa)
+
 
 export default router;
